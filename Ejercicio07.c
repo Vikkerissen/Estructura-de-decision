e2ca9038f9068b968a36d4a24cbbf60aafffd2b0
Vikkerissen <Vikkerissen32@gmail.com>
2017-03-26 23:12:56 -0300

    Update Ejercicio07.c

--- Ejercicio07.c
+++ Ejercicio07.c
@@ -1,3 +1,8 @@
+/*7-) Un fabricante de repuestos para tractores ha descubierto que ciertos artículos identificados por los
+números de catálogo 12121 al 18081; 30012 al 45565 y 67000 al 68000 son defectuosos.
+Desea que se confeccione un programa que informándole el número de catálogo indique si el artículo es o no defectuoso.
+Los artículos del catálogo van desde el 1200 al 90000.-*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
