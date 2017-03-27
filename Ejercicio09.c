5fddd058e3df6a04c94fd0b0977ec082c510df6f
Vikkerissen <Vikkerissen32@gmail.com>
2017-03-26 23:14:53 -0300

    Update Ejercicio09.c

--- Ejercicio09.c
+++ Ejercicio09.c
@@ -1,3 +1,10 @@
+/*9-) La farmacia Sindical efectúa descuentos a sus afiliados según el importe de la compra con la siguiente escala:
+a) menor de 55 $ el descuento es del 4,5 %
+b) entre 55 y 100 $ “ 8,0 %
+c) más de 100 $ el descuento es del 10,5%
+Confeccionar un programa que reciba un importe e informe : el precio ingresado, el descuento y el precio neto a cobrar,
+con mensajes aclaratorios. */
+
 #include <stdio.h>
 #include <stdlib.h>
 
